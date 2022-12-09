9196b322a2c17f0bcbb7a4fa570cdb0d52582f33
Satish Mishra <zicsxone@gmail.com>
2022-12-09 22:42:46 +0530

    added CHAT_OVER_CAKEBOOK

---
+++ CHAT_OVER_CAKEBOOK.py
@@ -0,0 +1,17 @@
+# x = input()
+# count = 0
+# for i in x:
+#     if i == "h":
+#         count = count + 1
+#     elif i == "e" and count == 1:
+#         count = count + 1
+#     elif i == "l" and count == 2:
+#         count = count + 1
+#     elif i == "l" and count == 3:
+#         count = count + 1
+#     elif i == "o" and count == 4:
+#         count = count + 1
+# if count == 5:
+#     print("YES")
+# else:
+#     print("NO")
