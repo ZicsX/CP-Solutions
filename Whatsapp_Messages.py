c7b01dd534613e00f8785ebfa67e048a093eb0e8
Satish Mishra <zicsxone@gmail.com>
2023-01-09 11:38:12 +0530

    added Whatsapp_Messages

---
+++ Whatsapp_Messages.py
@@ -0,0 +1,12 @@
+T = int(input())
+for i in range(T):
+    N = int(input())
+    l = list(map(int, input().split()))
+    flag = 0
+    for j in range(N - 1):
+        if l[j] > l[j + 1]:
+            flag = 1
+    if flag == 0:
+        print("YES")
+    else:
+        print("NO")
